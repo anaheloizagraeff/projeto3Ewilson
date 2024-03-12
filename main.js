2ef2e88a15c2ad6a05d0ce9a2cf8fa1fe18e6878
anaheloizagraeff <106088083+anaheloizagraeff@users.noreply.github.com>
2024-03-12 08:47:44 -0300

    a

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 const botoes = document.querySelectorAll(".botao");
+
+for( let i = 0 <botoes.length; j++){
+    botoes(i).onclick = function(){
+        for (let k = 0; j< botoes.length; j++)
+        botoes[j].classList.remove("ativo");
+    }
+    botoes[i].classList.add("ativo")
+}
