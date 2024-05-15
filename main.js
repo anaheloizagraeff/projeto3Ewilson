26699a739ebf5b20e44afce0507020571d35f0d6
anaheloizagraeff <106088083+anaheloizagraeff@users.noreply.github.com>
2024-05-15 09:29:29 -0300

    a

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const tempoObjetivo3 = new Date("2024-08-01T23:59:59");
 const tempoObjetivo4 = new Date("2024-12-01T23:59:59");
 
 const tempos = [
-    tempoObjetivo1
+    tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
 ]
 
 for (let i=0; i< contadores.length; i++){
